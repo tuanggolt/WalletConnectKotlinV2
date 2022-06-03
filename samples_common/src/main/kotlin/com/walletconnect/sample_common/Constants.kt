e44ef87dff3d74ef6f96e02fcfd15033a705978a
szymon <sz.rzad@gmail.com>
2022-06-03 17:39:14 +0200

    WIP

--- samples_common/src/main/kotlin/com/walletconnect/sample_common/Constants.kt
+++ samples_common/src/main/kotlin/com/walletconnect/sample_common/Constants.kt
@@ -1,4 +1,4 @@
 package com.walletconnect.sample_common
 
 const val WALLET_CONNECT_PROD_RELAY_URL = "relay.walletconnect.com"
-val PROJECT_ID = System.getenv("WC_CLOUD_PROJECT_ID") ?: ""
+const val PROJECT_ID = BuildConfig.PROJECT_ID
